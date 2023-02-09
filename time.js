3367ffadb2a2b3f5f6205d76b6af4706cc7bc6d0
jerrymiahdg <124826126+jerrymiahdg@users.noreply.github.com>
2023-02-09 14:14:10 -0800

    Add files via upload

---
+++ time.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//documentQuerySelector('.check').addEventListener(click, function() {
+//    if(documentQuerySelector('.task-name').value, documentQuerySelector('.time-start').value, documentQuerySelector('.time-end').value) {
+//        documentQuerySelecto('.name').textContent = documentQuerySelector('.task-name').value
+//        documentQuerySelecto('.time').textContent = (`${documentQuerySelector('.hour-start').value}:${documentQuerySelector('.minute-start').value} - ${documentQuerySelector('.hour-end').value}:${documentQuerySelector('.minute-end').value}`)
+//    }
+//    return;
+//})
+
