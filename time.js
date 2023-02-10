13f92fd6842070805360215ab9d2bbbc037e833f
jerrymiahdg <124826126+jerrymiahdg@users.noreply.github.com>
2023-02-09 19:35:38 -0800

    Update time.js

--- time.js
+++ time.js
@@ -1,10 +1,39 @@
-'use strict';
+'use strict'
 
-//documentQuerySelector('.check').addEventListener(click, function() {
-//    if(documentQuerySelector('.task-name').value, documentQuerySelector('.time-start').value, documentQuerySelector('.time-end').value) {
-//        documentQuerySelecto('.name').textContent = documentQuerySelector('.task-name').value
-//        documentQuerySelecto('.time').textContent = (`${documentQuerySelector('.hour-start').value}:${documentQuerySelector('.minute-start').value} - ${documentQuerySelector('.hour-end').value}:${documentQuerySelector('.minute-end').value}`)
-//    }
-//    return;
-//})
 
+document.getElementsByClassName('minute-start')[0].value = '00'
+document.getElementsByClassName('hour-start')[0].value = '00'
+document.getElementsByClassName('minute-end')[0].value = '00'
+document.getElementsByClassName('hour-end')[0].value = '00'
+document.getElementsByClassName('ampm-start')[0].addEventListener('click', function() {
+    if(document.getElementsByClassName('ampm-start')[0].textContent == 'AM') {
+        document.getElementsByClassName('ampm-start')[0].textContent = 'PM'
+    } else {
+        document.getElementsByClassName('ampm-start')[0].textContent = 'AM'
+    }
+})
+
+document.getElementsByClassName('ampm-end')[0].addEventListener('click', function() {
+    if(document.getElementsByClassName('ampm-end')[0].textContent == 'AM') {
+        document.getElementsByClassName('ampm-end')[0].textContent = 'PM'
+    } else {
+        document.getElementsByClassName('ampm-end')[0].textContent = 'AM'
+    }
+})
+
+document.getElementsByClassName('check')[0].addEventListener('click', function() {
+    if(
+        Number(document.getElementsByClassName('minute-start')[0].value) >= 0 &&
+        Number(document.getElementsByClassName('hour-start')[0].value) >= 1 &&
+        Number(document.getElementsByClassName('minute-end')[0].value) >= 0 &&
+        Number(document.getElementsByClassName('hour-end')[0].value) >= 1 &&
+        Number(document.getElementsByClassName('minute-start')[0].value) <= 59 &&
+        Number(document.getElementsByClassName('hour-start')[0].value) <= 12 &&
+        Number(document.getElementsByClassName('minute-end')[0].value) <= 59 &&
+        Number(document.getElementsByClassName('hour-end')[0].value) <= 12 &&
+        document.getElementsByClassName('task-name')[0].value
+    ) {
+        document.getElementsByClassName('name')[0].textContent = document.getElementsByClassName('task-name')[0].value
+        document.getElementsByClassName('time')[0].textContent = `${document.getElementsByClassName('hour-start')[0].value}:${document.getElementsByClassName('minute-start')[0].value} ${document.getElementsByClassName('ampm-start')[0].textContent} - ${document.getElementsByClassName('hour-end')[0].value}:${document.getElementsByClassName('minute-end')[0].value} ${document.getElementsByClassName('ampm-end')[0].textContent}`
+    }
+})
