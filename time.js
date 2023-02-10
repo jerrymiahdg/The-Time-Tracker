9140cc95c14ffa3351799a5a5826f59514566529
jerrymiahdg <124826126+jerrymiahdg@users.noreply.github.com>
2023-02-10 09:06:57 -0800

    Update time.js

--- time.js
+++ time.js
@@ -1,10 +1,10 @@
 'use strict'
 
 
-document.getElementsByClassName('minute-start')[0].value = '00'
-document.getElementsByClassName('hour-start')[0].value = '00'
-document.getElementsByClassName('minute-end')[0].value = '00'
-document.getElementsByClassName('hour-end')[0].value = '00'
+document.getElementsByClassName('minute-start')[0].value = ''
+document.getElementsByClassName('hour-start')[0].value = ''
+document.getElementsByClassName('minute-end')[0].value = ''
+document.getElementsByClassName('hour-end')[0].value = ''
 document.getElementsByClassName('ampm-start')[0].addEventListener('click', function() {
     if(document.getElementsByClassName('ampm-start')[0].textContent == 'AM') {
         document.getElementsByClassName('ampm-start')[0].textContent = 'PM'
@@ -21,6 +21,8 @@ document.getElementsByClassName('ampm-end')[0].addEventListener('click', functio
     }
 })
 
+document.getElementsByClassName('task')[0].style.visibility = 'hidden'
+
 document.getElementsByClassName('check')[0].addEventListener('click', function() {
     if(
         Number(document.getElementsByClassName('minute-start')[0].value) >= 0 &&
@@ -33,7 +35,31 @@ document.getElementsByClassName('check')[0].addEventListener('click', function()
         Number(document.getElementsByClassName('hour-end')[0].value) <= 12 &&
         document.getElementsByClassName('task-name')[0].value
     ) {
+        document.getElementsByClassName('task')[0].style.visibility = 'visible'
         document.getElementsByClassName('name')[0].textContent = document.getElementsByClassName('task-name')[0].value
         document.getElementsByClassName('time')[0].textContent = `${document.getElementsByClassName('hour-start')[0].value}:${document.getElementsByClassName('minute-start')[0].value} ${document.getElementsByClassName('ampm-start')[0].textContent} - ${document.getElementsByClassName('hour-end')[0].value}:${document.getElementsByClassName('minute-end')[0].value} ${document.getElementsByClassName('ampm-end')[0].textContent}`
+        document.getElementsByClassName('minute-start')[0].value = ''
+        document.getElementsByClassName('hour-start')[0].value = ''
+        document.getElementsByClassName('minute-end')[0].value = ''
+        document.getElementsByClassName('hour-end')[0].value = ''
+        document.getElementsByClassName('task-name')[0].value = ''
     }
 })
+
+document.getElementsByClassName('edit')[0].addEventListener('click', function() {
+    if(document.getElementsByClassName('task')[0].style.visibility == 'visible') {
+        document.getElementsByClassName('task-name')[0].value = document.getElementsByClassName('name')[0].textContent
+    }
+})
+
+document.getElementsByClassName('delete')[0].addEventListener('click', function() {
+    if(document.getElementsByClassName('task')[0].style.visibility == 'visible') {
+        document.getElementsByClassName('task')[0].style.visibility = 'hidden'
+        document.getElementsByClassName('name')[0].textContent = ''
+        document.getElementsByClassName('time')[0].textContent = ''
+    }
+})
+
+//let clone = document.getElementsByClassName('task').cloneNode(true);
+//clone.class = 'task2';
+//document.getElementsByClassName('task').after(clone);
