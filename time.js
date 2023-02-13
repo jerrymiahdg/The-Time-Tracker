455cd35472a81e13cbb93e4b1f5d88b3da70dc67
jerrymiahdg <124826126+jerrymiahdg@users.noreply.github.com>
2023-02-12 20:38:20 -0800

    Update time.js

--- time.js
+++ time.js
@@ -7,7 +7,15 @@ let timeEndMinute
 let ampmStart
 let ampmEnd
 let editing
+let numberOfTasks = 0
 
+const taskArray = []
+const nameArray = []
+const timeArray = []
+const editArray = []
+const deleteArray = []
+
+document.getElementsByClassName('og-task')[0].style.display = 'none'
 document.getElementsByClassName('minute-start')[0].value = ''
 document.getElementsByClassName('hour-start')[0].value = ''
 document.getElementsByClassName('minute-end')[0].value = ''
@@ -50,8 +58,34 @@ document.getElementsByClassName('check')[0].addEventListener('click', function()
         timeEndMinute = noZero('minute-end')
         ampmStart = document.getElementsByClassName('ampm-start')[0].textContent
         ampmEnd = document.getElementsByClassName('ampm-end')[0].textContent
-        document.getElementsByClassName('name')[0].textContent = document.getElementsByClassName('task-name')[0].value
-        document.getElementsByClassName('time')[0].textContent = `${(Number(document.getElementsByClassName('hour-start')[0].value)*10)/10}:${noZero('minute-start')} ${document.getElementsByClassName('ampm-start')[0].textContent} - ${(Number(document.getElementsByClassName('hour-end')[0].value)*10)/10}:${noZero('minute-end')} ${document.getElementsByClassName('ampm-end')[0].textContent}`
+        
+        
+        taskArray.push(document.createElement('div'))
+        nameArray.push(document.createElement('p'))
+        timeArray.push(document.createElement('p'))
+        editArray.push(document.createElement('button'))
+        deleteArray.push(document.createElement('button'))
+        
+        taskArray[taskArray.length - 1].className = ('task')
+        nameArray[nameArray.length - 1].className = ('name')
+        timeArray[timeArray.length - 1].className = ('time')
+        editArray[editArray.length - 1].className = ('btn edit')
+        deleteArray[deleteArray.length - 1].className = ('delete')
+        
+
+        taskArray[taskArray.length - 1].appendChild(nameArray[nameArray.length - 1])
+        taskArray[taskArray.length - 1].appendChild(timeArray[timeArray.length - 1])
+        taskArray[taskArray.length - 1].appendChild(editArray[editArray.length - 1])
+        taskArray[taskArray.length - 1].appendChild(deleteArray[deleteArray.length - 1])
+
+        document.getElementsByClassName('time-chart')[0].appendChild(taskArray[taskArray.length - 1])
+        
+        editArray[editArray.length - 1].innerHTML = 'Edit';
+        deleteArray[deleteArray.length - 1].innerHTML = 'X';
+
+        nameArray[nameArray.length - 1].innerHTML = document.getElementsByClassName('task-name')[0].value;
+        timeArray[timeArray.length - 1].innerHTML = `${(Number(document.getElementsByClassName('hour-start')[0].value)*10)/10}:${noZero('minute-start')} ${document.getElementsByClassName('ampm-start')[0].textContent} - ${(Number(document.getElementsByClassName('hour-end')[0].value)*10)/10}:${noZero('minute-end')} ${document.getElementsByClassName('ampm-end')[0].textContent}`;
+
         document.getElementsByClassName('minute-start')[0].value = ''
         document.getElementsByClassName('hour-start')[0].value = ''
         document.getElementsByClassName('minute-end')[0].value = ''
