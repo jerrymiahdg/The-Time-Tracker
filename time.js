c7bb9f04d0ef2c5c0693bf3e2f0efc47c7fbf47e
jerrymiahdg <124826126+jerrymiahdg@users.noreply.github.com>
2023-02-17 22:33:02 -0800

    Update time.js

--- time.js
+++ time.js
@@ -4,7 +4,7 @@ var ampmStart = document.getElementsByClassName('ampm-start')[0]
 var ampmEnd = document.getElementsByClassName('ampm-end')[0]
 var check = document.getElementsByClassName('check')[0]
 var taskInput = document.getElementsByClassName('task-input')[0]
-var taskName = document.getElementsByClassName('time-name')[0]
+var taskName = document.getElementsByClassName('task-name')[0]
 var timeStart = document.getElementsByClassName('time-start')[0]
 var timeEnd = document.getElementsByClassName('time-end')[0]
 var hourStart = document.getElementsByClassName('hour-start')[0]
@@ -17,6 +17,7 @@ var name = document.getElementsByClassName('name')[0]
 var time = document.getElementsByClassName('time')[0]
 var dlt = document.getElementsByClassName('delete')[0]
 var edit = document.getElementsByClassName('edit')[0]
+var close = document.getElementsByClassName('close')[0]
 const taskArray = []
 const nameArray = []
 const timeArray = []
@@ -26,10 +27,23 @@ const hourStartArray = []
 const minuteStartArray = []
 const hourEndArray = []
 const minuteEndArray = []
+let number = -1
+let editing = {boolean: false, number}
+
+task.style.display = 'none'
+taskInput.style.display = 'block'
 
 ampmStart.addEventListener('click', ampm(ampmStart))
 ampmEnd.addEventListener('click', ampm(ampmEnd))
-task.style.display = 'none'
+close.addEventListener('click', function() {
+    if(taskInput.style.display == 'block') {
+        taskInput.style.display = 'none'
+        close.textContent = 'v'
+    } else {
+        taskInput.style.display = 'block'
+        close.textContent = '^'
+    }
+})
 
 function ampm(i) {
     return function() {
@@ -39,33 +53,78 @@ function ampm(i) {
 }
 
 check.addEventListener('click', function() {
-    if(taskName.value !== '' && Number(hourStart.value) < 12 && Number(minuteStart.value) < 60 && Number(hourEnd.value) < 12 && Number(minuteEnd.value) < 60) {
+    if(taskName.value !== '' &&
+    hourStart.value !== '' &&
+    minuteStart.value !== '' &&
+    hourEnd.value !== '' &&
+    minuteEnd.value !== '' && 
+    Number(hourStart.value) < 12 && 
+    Number(minuteStart.value) < 60 && 
+    Number(hourEnd.value) < 12 && 
+    Number(minuteEnd.value) < 60
+    ) {
+        if(editing.boolean == false) {number = number + 1
         taskArray.push(document.createElement('div'))
-        taskArray[taskArray.length - 1].className = 'taskInput'
-        nameArray.push(document.createElemenet('p'))
-        nameArray[nameArray.length - 1].className = 'name'
-        nameArray[nameArray.length - 1].textContent = taskName.value
+        taskArray[number].className = 'task'
+        nameArray.push(document.createElement('p'))
+        nameArray[number].className = 'name'
+        nameArray[number].textContent = taskName.value
         timeArray.push(document.createElement('p'))
-        hourStartArray.push(hourStart.value)
-        minuteStartArray.push(minuteStart.value)
-        hourEndArray.push(hourEndStart.value)
-        minuteEndArray.push(minuteEndStart.value)
-        timeArray[timeArray.length - 1].className = 'time'
-        timeArray[timeArray.length - 1].textContent = `${hourStart.value}:${minuteStart.value} - ${hourEnd.value}:${minuteEnd.value}`
+        hourStartArray.push((Number(hourStart.value)*10)/10)
+        minuteStartArray.push(minute(minuteStart))
+        hourEndArray.push((Number(hourEnd.value)*10)/10)
+        minuteEndArray.push(minute(minuteEnd))
+        timeArray[number].className = 'time'
+        timeArray[number].textContent = `${(Number(hourStart.value)*10)/10}:${minute(minuteStart)} - ${(Number(hourEnd.value)*10)/10}:${minute(minuteEnd)}`
         dltArray.push(document.createElement('button'))
-        dltArray[dltArray.length - 1].className = 'delete'
+        dltArray[number].className = 'delete'
+        dltArray[number].textContent = 'X'
         editArray.push(document.createElement('button'))
-        editArray[editArray.length - 1].className = 'edit'
-        editFunc(taskArray.length - 1)
+        editArray[number].className = 'btn-2 edit'
+        editArray[number].textContent = 'Edit'
+        timeChart.appendChild(taskArray[number])
+        taskArray[number].appendChild(nameArray[number])
+        taskArray[number].appendChild(timeArray[number])
+        taskArray[number].appendChild(dltArray[number])
+        taskArray[number].appendChild(editArray[number])
+        editFunc(number)
+        dltFunc(number)} else {
+            nameArray[editing.number].textContent = taskName.value
+            hourStartArray[editing.number] = (Number(hourStart.value)*10)/10
+            minuteStartArray[editing.number] = (minute(minuteStart))
+            hourEndArray[editing.number] = (Number(hourEnd.value)*10)/10
+            minuteEndArray[editing.number] = (minute(minuteEnd))
+            timeArray[editing.number].textContent = `${(Number(hourStart.value)*10)/10}:${minute(minuteStart)} - ${(Number(hourEnd.value)*10)/10}:${minute(minuteEnd)}`
+        }
+        taskName.value = ''
+        hourStart.value = ''
+        minuteStart.value = ''
+        hourEnd.value = ''
+        minuteEnd.value = ''
     }
 })
 
 function editFunc(i) {
-    return function() {editArray[i].addEventListener('click', function() {
-        taskName.value = nameArray[i]
+    editArray[i].addEventListener('click', function() {
+        taskName.value = nameArray[i].textContent
         hourStart.value = hourStartArray[i]
         minuteStart.value = minuteStartArray[i]
         hourEnd.value = hourStartArray[i]
         minuteEnd.value = minuteEndArray[i]
-    })}
+        check.textContent = 'Edit Task'
+        editing.boolean = true
+        editing.number = i
+    })
+}
+
+function dltFunc(i) {
+    dltArray[i].addEventListener('click', function() {
+        taskArray[i].style.display = 'none'
+    })
+}
+
+function minute(i) {
+    if(Number(i.value) < 10 && i.value.length < 2) {
+        return '0' + i.value
+    } else {return i.value}
 }
